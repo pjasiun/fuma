f813ab6986975ea0f96f38575d917ee7660d4c72
Piotr Jasiun <p.jasiun@cksource.com>
2016-01-20 00:11:39 +0100

    Slack API usage scrach.

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 'use strict';
 
 const http = require( 'http' );
+const url  = require( 'url' );
 
 const PORT = 8080;
 
 const server = http.createServer( function( request, response ) {
-	response.end( 'It Works!!! Path: ' + request.url );
+	const urlParts = url.parse( request.url, true );
+
+	response.writeHead( 200, { 'Content-Type': 'application/json' } );
+	response.end( JSON.stringify( {
+		'response_type': 'in_channel',
+		'text': 'user: ' + urlParts.query.user_name + '\n',
+		'attachments': [
+			{
+				'text': 'Attachment!'
+			}
+		]
+	} ) );
 } );
 
 server.listen( PORT, function() {
