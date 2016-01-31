3748c1a8c427d7bc66a71d596615ab0efca0765f
Piotr Jasiun <p.jasiun@cksource.com>
2016-01-31 23:01:05 +0100

    Introduced `public` keyword.

--- app/controller.js
+++ app/controller.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const matchPublic = /^\s*public\s*(\S*)$/;
+
 class Controller {
 	constructor() {
 		this.commands = [];
@@ -12,10 +14,22 @@ class Controller {
 	}
 
 	handleRequest( request ) {
+		const values = matchPublic.exec( request.text );
+		let isPublic = false;
+
+		if ( values ) {
+			request.text = values[ 1 ];
+			isPublic = true;
+		}
+
 		for ( let command of this.commands ) {
 			const response = command.handleRequest( request );
 
 			if ( response ) {
+				if ( isPublic ) {
+					response.response_type = 'in_channel';
+				}
+
 				return response;
 			}
 		}
